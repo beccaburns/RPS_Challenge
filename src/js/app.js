f92abeed79c63792b921f577efb24721a1b1a434
Becca <beccaburns89@gmail.com>
2019-10-12 09:59:52 +0200

    Creates ComputerChoice function with Math.floor(Math.random) variable to allow computer to choose between array with rock, paper and scissors.

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,16 @@ let playerScore = document.getElementById('player-score')
 let computerScore = document.getElementById('computer-score')
 let scoreBoard = document.querySelector('.score-board')
 
+function getComputerChoice() {
+  const choices = ['r', 'p', 's']
+  const randomNumber = (Math.floor(Math.random() * 3 )
+  return choices[randomNumber]
+}
+
+function game(playerChoice) {
+  console.log(' ' + playerChoice)
+}
+
 function main() {
   paperButton.addEventListener('click',function() {
     game('p')
@@ -21,3 +31,5 @@ function main() {
     game('s')
   })
 }
+
+main()
