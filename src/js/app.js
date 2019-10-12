3873167b3abb89e55d0b5a315372b6f5e5b5c63a
Becca <beccaburns89@gmail.com>
2019-10-12 09:51:05 +0200

    Creates main function for rock paper scissors options.

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,23 @@
 let playerScore = 0;
 let computerScore = 0;
-let rockButton = document.getElementById('rock')
-let paperButton = document.getElementById('paper')
-let scissorsButton = document.getElementById('scissors')
+let rockButton = document.getElementById('r')
+let paperButton = document.getElementById('p')
+let scissorsButton = document.getElementById('s')
 let gameResult = document.getElementById('result')
+let playerScore = document.getElementById('player-score')
+let computerScore = document.getElementById('computer-score')
+let scoreBoard = document.querySelector('.score-board')
 
-function
+function main() {
+  paperButton.addEventListener('click',function() {
+    game('p')
+  })
+
+  rockButton.addEventListener('click',function() {
+    game('r')
+  })
+
+  scissorsButton.addEventListener('click',function() {
+    game('s')
+  })
+}
