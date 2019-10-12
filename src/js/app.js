595000cb0472caa9331bf986f9c94aecc57ae3a8
Becca <beccaburns89@gmail.com>
2019-10-12 09:18:18 +0200

    Updates HTML with div for result and scoreboard for user engagement.

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+let playerScore = 0;
+let computerScore = 0;
+let rockButton = document.getElementById('rock')
+let paperButton = document.getElementById('paper')
+let scissorsButton = document.getElementById('scissors')
+let gameResult = document.getElementById('result')
+
+function
